9c24b7ed7f341c9297e321d71b226abcd69831f6
tomasz.golkowski <tomaszgolkowski00@gmail.com>
2024-04-26 10:54:28 +0200

    Code refactor

--- src/nodeSubscriptionManager.ts
+++ src/nodeSubscriptionManager.ts
@@ -1,6 +1,6 @@
 import { Order } from "./types/Order";
 import { createPosition } from "./positionCreator";
-import { subClient, queryClient } from "./processorSubscriptionClient";
+import { queryClient } from "./processorSubscriptionClient";
 import { MarketData, OrderData } from "./types/graphql";
 
 export const subscribeToMarkets = async () => {
@@ -28,23 +28,23 @@ export const subscribeToMarkets = async () => {
   };
 };
 
-const marketSubscriptionMap: Map<string, any> = new Map();
-const manageMarkets = (markets: string[]) => {
-  console.log(markets);
-  markets.forEach((marketId: string) => {
-    if (!marketSubscriptionMap.has(marketId)) {
-      marketSubscriptionMap.set(marketId, subscribeToOrders(marketId));
+const marketSubscriptionMap : Map<string, any> = new Map();
+const manageMarkets = (markets : string[]) => {
+    console.log(markets)
+    markets.forEach((marketId: string) => {
+        if (!marketSubscriptionMap.has(marketId)) {
+            marketSubscriptionMap.set(marketId, subscribeToOrders(marketId))
+        }
+    });
+    marketSubscriptionMap.forEach((value: any[], key: string) => {
+        if (Array.isArray(value) && !markets.includes(key)) {
+        value.forEach((unsubscribe: () => void) => {
+            unsubscribe();
+        });
+        marketSubscriptionMap.delete(key);
     }
-  });
-  marketSubscriptionMap.forEach((value: any[], key: string) => {
-    if (Array.isArray(value) && !markets.includes(key)) {
-      value.forEach((unsubscribe: () => void) => {
-        unsubscribe();
-      });
-      marketSubscriptionMap.delete(key);
-    }
-  });
-};
+    });
+}
 
 const subscribeToOrders = async (marketId: string) => {
   console.log(`Subscribing to orders for market ${marketId}`);
@@ -71,8 +71,8 @@ const subscribeToOrders = async (marketId: string) => {
   return () => {};
 };
 const getOrderBookOverlap = async (marketId: string) => {
-  const cheapestShortOrder = await queryClient.request(
-    `query orders {
+    const cheapestShortOrder = await queryClient.request(
+            `query orders {
                 orders(
                     where: {
                         market: { id_eq: "${marketId}" },
@@ -85,11 +85,11 @@ const getOrderBookOverlap = async (marketId: string) => {
                     price
                 }
             }`
-  );
-
-  console.log(`${marketId} | Cheapest short order: `, cheapestShortOrder);
-  const mostExpensiveLongOrder = await queryClient.request(
-    `query orders {
+        )
+        
+        console.log(`${marketId} | Cheapest short order: `, cheapestShortOrder);
+        const mostExpensiveLongOrder = await queryClient.request( 
+            `query orders {
                 orders(
                     where: {
                         market: { id_eq: "${marketId}" },
@@ -102,17 +102,11 @@ const getOrderBookOverlap = async (marketId: string) => {
                     price
                 }
             }`
-  );
-
-  console.log(
-    `${marketId} | Most expensive long order:  `,
-    mostExpensiveLongOrder
-  );
-  if (
-    mostExpensiveLongOrder !== undefined &&
-    cheapestShortOrder !== undefined
-  ) {
-    return await queryClient.request(`
+        )
+        
+        console.log(`${marketId} | Most expensive long order:  `, mostExpensiveLongOrder);
+        if(mostExpensiveLongOrder !== undefined && cheapestShortOrder !== undefined) {	
+            return await queryClient.request(`
                 query orders {
                     orders(
                         where: {
@@ -130,64 +124,49 @@ const getOrderBookOverlap = async (marketId: string) => {
                         who
                     }
                 }
-            `);
-  } else {
-    return undefined;
-  }
-};
+            `)
+        } else { return undefined; }
+}
 
 const manageOrdersChange = async (marketId: string) => {
-  console.log(`${marketId} | Order change detected`);
-  const overlappingOrders = await getOrderBookOverlap(marketId);
-  if (overlappingOrders) {
-    await manageOrders((overlappingOrders as any).orders, marketId);
-  }
-};
-
-const manageOrders = async (
-  values: { id: string; price: bigint; who: string; side: string }[],
-  marketId: string
-) => {
-  const orders = values.map((value) => {
-    return new Order(value.id, value.price, value.who, value.side);
-  });
-
-  const sortedLongOrderCollection = getOrdersPerSideSortedByPrice(
-    orders,
-    "LONG"
-  );
-  const sortedShortOrderCollection = getOrdersPerSideSortedByPrice(
-    orders,
-    "SHORT"
-  );
-
-  for (const nextLong of sortedLongOrderCollection) {
-    for (const shortOrder of sortedShortOrderCollection) {
-      if (
-        shortOrder.who !== nextLong.who &&
-        Number(shortOrder.price) <= Number(nextLong.price)
-      ) {
-        console.log(nextLong);
-        try {
-          await createPosition(marketId, shortOrder.id, nextLong.id);
-        } catch (error) {
-          console.error(error);
-          process.exit(0);
-        }
-      }
+    console.log(`${marketId} | Order change detected`);
+    const overlappingOrders = await getOrderBookOverlap(marketId); 
+    if(overlappingOrders) {
+        await manageOrders((overlappingOrders as any).orders, marketId);
     }
-  }
-};
+}
 
-const getOrdersPerSideSortedByPrice = (
-  orders: Order[],
-  side: string
-): Set<Order> => {
-  return new Set(
-    orders
-      .filter((order) => order.side === side)
-      .sort((a: Order, b: Order) => {
-        return a.price.toString().localeCompare(b.price.toString());
-      })
-  );
-};
+const manageOrders = async (values: { id: string, price: bigint, who: string, side: string }[], marketId: string) => {
+
+    const orders = values.map(value => { 
+        return new Order(value.id, value.price, value.who, value.side) 
+    });
+
+    const sortedLongOrderCollection = getOrdersPerSideSortedByPrice(orders, 'LONG');
+    const sortedShortOrderCollection = getOrdersPerSideSortedByPrice(orders, 'SHORT');
+  
+    for(const nextLong of sortedLongOrderCollection) {
+        for(const shortOrder of sortedShortOrderCollection) {
+            if (
+              shortOrder.who !== nextLong.who &&
+              Number(shortOrder.price) <= Number(nextLong.price)
+            ) {
+              console.log(nextLong);
+              try {
+                await createPosition(marketId, shortOrder.id, nextLong.id);
+              } catch (error) {
+                console.error(error);
+                process.exit(0);
+              }
+            }
+        }
+    }
+}
+
+const getOrdersPerSideSortedByPrice = (orders: Order[], side: string) : Set<Order> => {
+    return new Set(orders
+        .filter(order => order.side === side)
+        .sort((a : Order, b : Order) => { 
+            return a.price.toString().localeCompare(b.price.toString());
+        }));
+}
